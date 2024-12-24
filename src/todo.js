4bed849f10bed416e5c22609aa5336f97ccea7b6
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-24 14:13:44 +0300

    updated

---
+++ src/todo.js
@@ -0,0 +1,25 @@
+import { ref, onMounted } from "vue";
+
+const todosFunction = () => {
+  // Data
+  const todosList = ref([]);
+  // Method
+  // Set to Local Storage
+  const addToLocalSt = () => {
+    localStorage.setItem("todos", JSON.stringify(todosList.value));
+  };
+
+  const UpdateTodos = () => {
+    if (localStorage.getItem("todos")) {
+      todosList.value = JSON.parse(localStorage.getItem("todos"));
+    }
+    console.log(todosList.value);
+  };
+  onMounted(() => {
+    UpdateTodos();
+  });
+
+  return { todosList, addToLocalSt };
+};
+
+export default todosFunction();
